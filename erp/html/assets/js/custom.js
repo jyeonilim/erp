a346f40b098edace1a0699f8ba3f245fb744e31f
jyeon <jyeonilim@gmail.com>
2017-04-11 12:13:02 +0900

    erp 대여업무 완료

--- erp/html/assets/js/custom.js
+++ erp/html/assets/js/custom.js
@@ -35,5 +35,23 @@ $(document).ready(function() {
         $('.tab-left-card').parent('.slimScrollDiv').height(rh-330);
     });
 
+    var offset = 300,
+        offset_opacity = 1200,
+        scroll_top_duration = 700,
+        $back_to_top = $('.btn-top');
+    $(window).scroll(function(){
+        ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('is-visible') : $back_to_top.removeClass('is-visible fade-out');
+        if( $(this).scrollTop() > offset_opacity ) {
+            $back_to_top.addClass('fade-out');
+        }
+    });
+    $back_to_top.on('click', function(event){
+        event.preventDefault();
+        $('body,html').animate({
+                scrollTop: 0 ,
+            }, scroll_top_duration
+        );
+    });
+
 
 });
